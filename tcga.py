1158644f450f22d57e0e2c8499b765ac1dc2f8c3
Shivaal Roy <shivaal@stanford.edu>
2015-11-27 16:18:44 -0800

    progress on data pipeline

--- tcga.py
+++ tcga.py
@@ -1,9 +1,21 @@
 # See https://confluence.broadinstitute.org/display/GDAC/fbget#main-content.wiki-content
 # Also see http://firebrowse.org/api-docs/
+import json
 import firebrowse
 
-# example query of fbget on EGFR's mRNAseq information in UCS cohort
-print  firebrowse.Samples().mRNASeq(gene="egfr", cohort="ucs")
+# example query of firebrowse on EGFR's mRNAseq information in UCS cohort
+# firebrowse.Samples().mRNASeq(gene="egfr", cohort="ucs", format="json")
 
-# we can additionally get this data in JSON format
-# 
+datatypes = ["CopyNumber","RPPA","mRNA","miRNA","Methylation"]
+
+dates = "2015_08_21,2015_06_01,2015_04_02,2015_02_04,2014_12_06,2014_10_17,2014_09_02,2014_07_15,2014_05_18,2014_04_16,2014_03_16"
+
+text = firebrowse.Archives().StandardData(cohort="brca", date=dates, data_type=datatypes[0], page="1", page_size="2000")
+
+data = json.loads(text)
+
+print data['StandardData'][0]['urls'][0]
+
+f = open('data.json', 'w')
+f.write(text)
+f.close()
