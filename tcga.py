24925688f6e888679b35671e2bec12adfd3dca0d
Shivaal Roy <shivaal@stanford.edu>
2015-11-30 22:05:36 -0800

    filter out ffpe

--- tcga.py
+++ tcga.py
@@ -6,16 +6,18 @@ import firebrowse
 # example query of firebrowse on EGFR's mRNAseq information in UCS cohort
 # firebrowse.Samples().mRNASeq(gene="egfr", cohort="ucs", format="json")
 
-datatypes = ["CopyNumber","RPPA","mRNA","miRNA","Methylation"]
+datatypes = ["CopyNumber","RPPA","mRNA","miR","Methylation"]
 
-dates = "2015_08_21,2015_06_01,2015_04_02,2015_02_04,2014_12_06,2014_10_17,2014_09_02,2014_07_15,2014_05_18,2014_04_16,2014_03_16"
-
-text = firebrowse.Archives().StandardData(cohort="brca", date=dates, data_type=datatypes[0], page="1", page_size="2000")
+text = firebrowse.Archives().StandardData(cohort="brca", date='2015_06_01', data_type='CopyNumber,RPPA,mRNA,miR,Methylation', page="1", page_size="2000")
 
 data = json.loads(text)
 
-print data['StandardData'][0]['urls'][0]
+f1 = open('data1.json', 'w')
+f1.write(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
+f1.close()
 
-f = open('data.json', 'w')
-f.write(text)
-f.close()
+data["StandardData"] = [obj for obj in data["StandardData"] if not obj["sample_prep"] == "ffpe"]
+
+f2 = open('data2.json', 'w')
+f2.write(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
+f2.close()
