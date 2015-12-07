035796c0e414519c97b37f8224b0159686ce721a
Shivaal Roy <shivaal@stanford.edu>
2015-12-07 02:16:18 -0800

    add rpy2 to requirements.txt

--- process_files.py
+++ process_files.py
@@ -1,5 +1,6 @@
 import json
 import os
+import rpy2.robjects as robjects
 
 with open('filenames.json') as data_file:
   filenames = json.load(data_file)
@@ -8,3 +9,13 @@ for dirname in filenames:
 	for fname in os.listdir(dirname[0:-7]):
 	  if fname.endswith(".txt") and "MANIFEST" not in fname:
 	    print(fname)
+
+robjects.r('''
+        f <- function(r, verbose=FALSE) {
+            if (verbose) {
+                cat("I am calling f().\n")
+            }
+            2 * pi * r
+        }
+        f(3)
+        ''')
